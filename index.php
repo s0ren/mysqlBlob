dfc3fe15dc456b6ccfd706d3516d71ad9c55ea7b
Søren Magnusson <soren@magnussons.dk>
2016-06-21 23:17:52 +0200

    oprydning og connect i dbConnect.php

--- index.php
+++ index.php
@@ -1,25 +1,5 @@
 <?php
-    var_dump($_SERVER);
-    
-    if ( isset($_SERVER['SERVER_NAME'])) 
-    {
-        if($_SERVER['SERVER_NAME'] == 'localhost')
-        {
-            $dbHost = "localhost";
-            $dbUser = "root";
-            $dbPassword = "";
-            $dbName = "blob";
-        }
-        elseif ($_SERVER['SERVER_NAME'] == 'hundeprut.herokuapp.com') 
-        {
-            $dbHost = "m60mxazb4g6sb4nn.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-            $dbUser = "j8k40mipq1w2czqu";
-            $dbPassword = "bfa50wwhim2o6pau";
-            $dbName = "l8ryl26yzasnwqrd";
-        }
-        $db = mysqli_connect($dbHost, $dbUser, $dbPassword, $dbName) 
-                OR die(mysqli_connect_error()); 
-    }
+    require_once './dbConnect.php';
 ?>
 <!DOCTYPE html>
 <?php
@@ -30,7 +10,7 @@
         $image = addslashes(file_get_contents($_FILES['images']['tmp_name']));
         //you keep your column name setting for insertion. I keep image type Blob.
         $query = "INSERT INTO products (id,image) VALUES('','$image')";  
-        $qry = mysqli_query($db, $query) 
+        $qry = mysqli_query($dbCon, $query) 
                 OR die($query . mysqli_error($db));
     }
     
@@ -50,10 +30,9 @@
         </form>
         
         <?php
-        //$db = mysqli_connect("localhost","root","","blob"); //keep your db name
         $sql = "SELECT * FROM products ORDER BY id DESC";
-        $sth = $db->query($sql);
-        while($row=mysqli_fetch_array($sth))
+        $result = $dbCon->query($sql);
+        while($row=mysqli_fetch_array($result))
         {
             echo '<img height="200" src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>';
         }
