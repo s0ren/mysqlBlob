9d43cf6cfb6c2733b83ffe1bb3626e41910f5a5e
Søren Magnusson <soren@magnussons.dk>
2016-06-22 06:26:44 +0200

    Nu med smart connection

--- indeW.php
+++ indeW.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <?php
+    require_once './dbConnect.php';
 
     include_once './vendor/autoload.php';
     use \WideImage\WideImage;
@@ -7,14 +8,13 @@
     
     if (isset($_POST['submit']))
     {
-        $db = mysqli_connect("localhost","root","","blob"); //keep your db name
         //$image = addslashes(file_get_contents($_FILES['images']['tmp_name']));
         $image = WideImage::loadFromUpload('images');
         
         $data = addslashes($image->resize(NULL, 200)->asString('jpg', 80));
         //you keep your column name setting for insertion. I keep image type Blob.
         $query = "INSERT INTO products (id,image) VALUES('','$data')";  
-        $qry = mysqli_query($db, $query);
+        $qry = mysqli_query($dbCon, $query);
     }
     
 ?>
@@ -33,9 +33,8 @@
         </form>
         
         <?php
-        $db = mysqli_connect("localhost","root","","blob"); //keep your db name
         $sql = "SELECT * FROM products ORDER BY id DESC";
-        $sth = $db->query($sql);
+        $sth = $dbCon->query($sql);
         while($row=mysqli_fetch_array($sth))
         {
             echo '<img height="200" src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>';
